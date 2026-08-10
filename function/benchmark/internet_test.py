ca4470ce9181686bc1f4222ef75b3b511a82e8af
Sparsh Jha <sparshjha40@gmail.com>
2026-08-10 09:53:55 -0700

    Updated vm config to use standard lambda format; updated internet_test to also add body to SAAF response.

--- function/benchmark/internet_test.py
+++ function/benchmark/internet_test.py
@@ -1,6 +1,14 @@
 def internet_check(event, context):
     import urllib.request, socket, time
+    from .Inspector import Inspector
+    inspector = Inspector()
+    inspector.inspectAll()
     t0 = time.time()
     ip = socket.gethostbyname("example.com")          # tests DNS
     body = urllib.request.urlopen("https://example.com", timeout=10).read()
-    return {"dns": ip, "bytes": len(body), "ms": round((time.time()-t0)*1000)}
+    inspector.addAttribute("dns", ip)
+    inspector.addAttribute("bytes", len(body))
+    inspector.addAttribute("ms", round((time.time()-t0)*1000))
+    inspector.addAttribute("body", "".join(body.decode().splitlines()[:10]))
+    inspector.inspectAllDeltas()
+    return inspector.finish()
